42b546bab8865565890ed59681d187ed814306fb
MarieSainte <90503071+MarieSainte@users.noreply.github.com>
2021-10-17 19:48:15 +0200

    Add files via upload

---
+++ NeighbourServiceTest.java
@@ -0,0 +1,79 @@
+package com.openclassrooms.entrevoisins.service;
+
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test on Neighbour service
+ */
+@RunWith(JUnit4.class)
+public class NeighbourServiceTest {
+
+    private NeighbourApiService service;
+    private DummyNeighbourApiService dummyService;
+
+    @Before
+    public void setup() {
+        service = DI.getNewInstanceApiService();
+    }
+
+    @Test
+    public void getNeighboursWithSuccess() {
+        List<Neighbour> neighbours = service.getNeighbours();
+        List<Neighbour> expectedNeighbours = DummyNeighbourGenerator.DUMMY_NEIGHBOURS;
+        assertThat(neighbours, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedNeighbours.toArray()));
+    }
+
+    @Test
+    public void deleteNeighbourWithSuccess() {
+        Neighbour neighbourToDelete = service.getNeighbours().get(0);
+        service.deleteNeighbour(neighbourToDelete);
+        assertFalse(service.getNeighbours().contains(neighbourToDelete));
+    }
+
+    @Test
+    public void getFavoriteNeighboursWithSuccess() {
+        assertTrue(service.getFavoriteNeighbours().isEmpty());
+        Neighbour favoriteNeighbour = service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(favoriteNeighbour);
+        assertTrue(service.getFavoriteNeighbours().contains(favoriteNeighbour));
+    }
+
+    @Test
+    public void createNeighbourWithSuccess(){
+        Neighbour neighbourTest = new Neighbour(1, "Jordan", "https://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint-Pierre-du-Mont ; 5km",
+                "+33 6 86 57 90 14",  "Bonjour !Je souhaiterais faire de la marche.");
+        service.createNeighbour(neighbourTest);
+        assertTrue(service.getNeighbours().contains(neighbourTest));
+    }
+
+    @Test
+    public void addFavoriteNeighbourWithSuccess() {
+        Neighbour favoriteNeighbourTest = service.getNeighbours().get(1);
+        assertFalse(service.getFavoriteNeighbours().contains(favoriteNeighbourTest));
+        service.addFavoriteNeighbour(favoriteNeighbourTest);
+        assertTrue(service.getFavoriteNeighbours().contains(favoriteNeighbourTest));
+    }
+
+    @Test
+    public void deleteFavoriteNeighbourWithSuccess() {
+        Neighbour favoriteNeighbourTest = service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(favoriteNeighbourTest);
+        assertTrue(service.getFavoriteNeighbours().contains(favoriteNeighbourTest));
+        service.deleteFavoriteNeighbour(favoriteNeighbourTest);
+        assertFalse(service.getFavoriteNeighbours().contains(favoriteNeighbourTest));
+    }
+}
